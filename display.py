12518fb81647fcb22c675f4090b61e25d960ecdc
krishnakantgunjal <krishnagunjal2003@gmail.com>
2024-12-31 15:59:21 +0530

    Delete display.py

--- display.py
+++
@@ -1,7 +0,0 @@
-def print_timetable(timetable, class_name):
-    print(f"Timetable for {class_name}:\n")
-    print("Time      | " + " | ".join(timetable['9am'].keys()))
-    print("-------------------------------------------------")
-    for time, schedule in timetable.items():
-        row = f"{time:8} | " + " | ".join(task if task else "No class" for task in schedule.values())
-        print(row)
